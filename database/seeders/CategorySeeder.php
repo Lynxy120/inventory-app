02254db52802a09b99390d749d9d61d57d077c7a
Lynxy120 <u.boruto120806@gmail.com>
2026-04-21 13:20:21 +0700

    first commit

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $categories = ['Elektronik', 'Fashion', 'Makanan', 'Alat Tulis'];
+        foreach ($categories as $cat) {
+            Category::create(['name' => $cat]);
+        }
+    }
+}
